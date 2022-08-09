bafc9b554751a125b489c1d6bc0cf5729a2f10ca
uqilenan <uqilenan@wiener.hpc.dc.uq.edu.au>
2022-08-09 12:15:29 +1000

    + model for testing mapping in velocity

---
+++ models/multiphase/d3q27_pf_velocity_scaling/conf.mk
@@ -0,0 +1,12 @@
+ADJOINT=0
+TEST=FALSE
+OPT="q27*ML*OutFlow*time*altContactAngle*BGK*thermo*planarBenchmark*autosym"
+# q27 - Q27 lattice structure for phasefield
+# ML  - export densities for machine learning
+# OutFlow - include extra velocity stencil for outflowing boundaries
+# altContactAngle - geometric contact angle implementation, implemented by dmytro merged into code by travis
+# BGK - single relaxation time operator
+# thermo - include energy equation solver for temperature field, influences through
+#        - the surface tension
+# planarBenchmark - thermocapillary benchmark case
+# autosym - symmetry boundary conditions
